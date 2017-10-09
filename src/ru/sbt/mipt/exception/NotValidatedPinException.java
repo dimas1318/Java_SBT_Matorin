c051e2ced78b31d66a63a83a1a450717b5b57c62
dimas1318 <parshin@phystech.edu>
2017-10-09 13:03:03 +0300

    09.10.2k17

--- src/ru/sbt/mipt/exception/NotValidatedPinException.java
+++
@@ -1,4 +0,0 @@
-package ru.sbt.mipt.exception;
-
-public class NotValidatedPinException extends Throwable {
-}
--- src/ru/sbt/mipt/exception/WrongSumException.java
+++
@@ -1,4 +0,0 @@
-package ru.sbt.mipt.exception;
-
-public class WrongSumException extends Throwable {
-}
